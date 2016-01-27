6821d2f31678225948159552be82e99b28dd828b
sunxu3074 <393353762@qq.com>
2016-01-28 00:56:14 +0800

    添加根据色度饱和度及亮度生成新的bitmap的方法

--- sutils/src/main/java/xyz/isunxu/sutils/BitmapUtils.java
+++ sutils/src/main/java/xyz/isunxu/sutils/BitmapUtils.java
@@ -3,6 +3,10 @@ package xyz.isunxu.sutils;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.view.View;
 import android.view.Window;
 import java.io.ByteArrayOutputStream;
@@ -35,6 +39,42 @@ public class BitmapUtils {
         view.setDrawingCacheEnabled(true);
         view.buildDrawingCache();
         return view.getDrawingCache();
+    }
+
+    /**
+     * @param bm
+     * @param tone 色调
+     * @param saturation 饱和度
+     * @param lum 亮度
+     * @return 生成新的bitmap
+     */
+    public static Bitmap handleImageMatrix(Bitmap bm,float tone,float saturation,float lum) {
+        Bitmap bitmap = Bitmap.createBitmap(bm.getWidth(), bm.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+
+        // 色调
+        ColorMatrix toneMatrix = new ColorMatrix();
+        toneMatrix.setRotate(0, tone);
+        toneMatrix.setRotate(1, tone);
+        toneMatrix.setRotate(2, tone);
+
+        // 饱和度
+        ColorMatrix saturationMatrix = new ColorMatrix();
+        saturationMatrix.setSaturation(saturation);
+
+        // 亮度
+        ColorMatrix lumMatrix = new ColorMatrix();
+        lumMatrix.setScale(lum, lum, lum, 1);
+
+        ColorMatrix imageMatrix = new ColorMatrix();
+        imageMatrix.postConcat(toneMatrix);
+        imageMatrix.postConcat(saturationMatrix);
+        imageMatrix.postConcat(lumMatrix);
+
+        paint.setColorFilter(new ColorMatrixColorFilter(imageMatrix));
+        canvas.drawBitmap(bm,0,0,paint);
+        return bitmap;
     }
 }
 
